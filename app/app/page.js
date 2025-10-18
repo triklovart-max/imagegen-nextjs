ffeff86ec6756b4c78cf920561c84b2e6ecb5f70
triklovart-max <triklovart@gmail.com>
2025-10-19 01:54:08 +0700

    Delete app/app directory

--- app/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{ textAlign: "center", padding: "60px" }}>
-      <h1>Genered Image Karya Prasetyo</h1>
-      <p>Selamat datang! Website ini akan menampilkan hasil generate gambar AI.</p>
-    </main>
-  );
-}
